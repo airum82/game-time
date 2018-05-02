78687ef9fe0e454cdb0b297ef6fed8ecf605e034
Arram Mandel <airum82@gmail.com>
2018-05-02 14:26:22 -0600

    Repaired npm issue and started writing unit tests

---
+++ test/index.js
@@ -0,0 +1,9 @@
+const chai = require('chai');
+const assert = chai.assert;
+const Froggy = require('../lib/Froggy.js');
+
+describe('Froggy', function() {
+  it('should return true', function() {
+    assert.equal(true, true);
+  })
+})
