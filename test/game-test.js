4c6e11de5dc68a49543d4e53ac070a1ad94f490e
Arram Mandel <airum82@gmail.com>
2018-05-08 11:38:47 -0600

    Deleted a couple lines of unnecessary DOM manipulation

--- test/game-test.js
+++ test/game-test.js
@@ -5,34 +5,8 @@ const Loggy = require('../lib/Loggy.js');
 const Car = require('../lib/Car.js');
 const Game = require('../lib/game.js');
 
-// describe('Game', () => {
-//     it('should instantiate a new game with all default properties', () => {
-//         const game = new Game();
-
-        
-//         });
-//     });
-
-//     it('should create a new frog', () => {
-//         const game = new Game();
-
-//         assert.isObject(game.frogger);
-//     });
-
-//     it('can draw objects', () => {
-//         const game = new Game();
-
-//         assert.isFunction(game.draw);
-//     });
-
-    // it('lose a life if there is a collision with a car', () => {
-    //     const game = new Game();
-
-    //     game.roadKill = () => ;
-    //     game.death = () => ;
-    //     game.gameOver() =>;
-    //     game.checkForRoadCollision();
-
-    //     assert.equal(game.frogger.lives, 2);
-    // });
-   
+describe('Game', () => {
+    it('should instantiate a new game with all default properties', () => {
+        const game = new Game();
+  });
+});
