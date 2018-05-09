0c9bf5eb5828458a97881b548e5cff1872b61e15
Chris Boylen <billpolster@gmail.com>
2018-05-09 14:44:16 -0600

    Add tests to Froggy-test.js

--- test/Froggy-test.js
+++ test/Froggy-test.js
@@ -4,14 +4,13 @@ const Froggy = require('../lib/Froggy.js');
 const Character = require('../lib/Character.js');
 
 describe('Froggy', function () {
-    it('should return true', function () {
-        assert.equal(true, true);
+    let froggy;
+    beforeEach(() => {
+        froggy = new Froggy(10, 10, 10, 10, 10, 2)
     });
 
     it('should instantiate a new froggy with all default properties', () => {
-        const froggy = new Froggy(10, 10, 10, 10, 10, 2);
-
-        assert.isObject(froggy);
+       assert.isObject(froggy);
         assert.equal(froggy.img, 10);
         assert.equal(froggy.x, 10);
         assert.equal(froggy.y, 10);
@@ -20,27 +19,27 @@ describe('Froggy', function () {
         assert.equal(froggy.velocity, 2);
     });
 
-    it('should have inherited the move method from character class', () => {
-        var froggy = new Froggy();
-        
-        assert.isFunction(froggy.move);
-    })
-
     it('should have the draw', () => {
-        var froggy = new Froggy();
-
         assert.isFunction(froggy.draw);
     })
 
     it('should have the death method', () => {
-        var froggy = new Froggy();
-
         assert.isFunction(froggy.death);
     })
 
-    it('should have the scorePoint method', () => {
-        var froggy = new Froggy();
+    it('death method should make froggy lose a life', () => {
+        assert.equal(froggy.lives, 5);
+        froggy.death();
+        assert.equal(froggy.lives, 4);
+    })
 
-        assert.isFunction(froggy.scorePoint);
+    it('should have the scorePoint method', () => {
+       assert.isFunction(froggy.scorePoint);
+    })
+
+    it('score method should increase froggy score', () => {
+        assert.equal(froggy.score, 0);
+        froggy.scorePoint();
+        assert.equal(froggy.score, 1);
     })
 })
